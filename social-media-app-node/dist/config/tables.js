d76b4a6b0ae704f80a57edc746d397e34507cd75
BansiPatel84 <bansiben.patel@neosofttech.com>
2020-11-02 10:43:41 +0530

    changes

--- social-media-app-node/dist/config/tables.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Tables = void 0;
-class Tables {
-}
-exports.Tables = Tables;
-// List tables created in database
-Tables.USER = "user";
-//# sourceMappingURL=tables.js.map
